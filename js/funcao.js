9e58883e539f1ff697c5aee8011e811d39a64114
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:53:06 -0300

    Adicionando arquivos de função

---
+++ js/funcao.js
@@ -0,0 +1,16 @@
+function teste() {
+    console.log("Oláááa");
+}
+function meunome(name) {
+    console.log(`Olá ${name}`);
+}
+
+meunome("Larissa");
+meunome("Estella");
+meunome("Douglas");
+
+function calc(arg1, agr2) {
+    return arg1 + agr2;
+}
+
+console.log(calc(4, 4));
