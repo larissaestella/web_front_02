3b60b00734b77f8399ce4c85f0d2edb912f42564
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-17 11:15:13 -0300

    subindo arquivos modal

---
+++ modal.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+const poupup = document.querySelector(".poupup-wrapper");
+
+button.addEventListener("click", function() {
+    poupup.classList.add("d-block");
+})
+
